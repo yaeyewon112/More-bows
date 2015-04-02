c2cf0fb6f3b20501b3d2f3f0cd9b788cd45d0d29
Darren Boon <darren.boon@rogers.com>
2015-04-02 09:49:00 -0400

    Added gold and diamond bows.

--- src/main/java/com/lucidsage/morebows/Item/Bow.java
+++ src/main/java/com/lucidsage/morebows/Item/Bow.java
@@ -3,18 +3,29 @@ package com.lucidsage.morebows.item;
 import com.lucidsage.morebows.MoreBows;
 import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
 import net.minecraft.item.*;
+import net.minecraft.stats.StatList;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class Bow extends ItemBow implements IBow {
 
     String name;
     Object[] recipe;
+    int enchantability;
+    float drawSpeed;
 
-    public Bow(String inName, int damage, Object[] inRecipe)
+    public Bow(String inName, int damage, float inDrawSpeed, int inEnchantability, Object[] inRecipe)
     {
         name = inName;
         recipe = inRecipe;
+        drawSpeed = inDrawSpeed;
+        enchantability = inEnchantability;
         this.setMaxDamage(damage);
         this.setUnlocalizedName(name);
     }
@@ -31,4 +42,81 @@ public class Bow extends ItemBow implements IBow {
         itemModelMesher.register(this, 0, new ModelResourceLocation(MoreBows.MODID + ":" + name, "inventory"));
     }
 
+    @Override
+    public int getItemEnchantability()
+    {
+        return enchantability;
+    }
+
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, EntityPlayer playerIn, int timeLeft)
+    {
+        int j = this.getMaxItemUseDuration(stack) - timeLeft;
+        net.minecraftforge.event.entity.player.ArrowLooseEvent event = new net.minecraftforge.event.entity.player.ArrowLooseEvent(playerIn, stack, j);
+        if (net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event)) return;
+        j = event.charge;
+
+        boolean flag = playerIn.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, stack) > 0;
+
+        if (flag || playerIn.inventory.hasItem(Items.arrow))
+        {
+            float f = (float)j / drawSpeed;
+            f = (f * f + f * 2.0F) / 3.0F;
+
+            if ((double)f < 0.1D)
+            {
+                return;
+            }
+
+            if (f > 1.0F)
+            {
+                f = 1.0F;
+            }
+
+            EntityArrow entityarrow = new EntityArrow(worldIn, playerIn, f * 2.0F);
+
+            if (f == 1.0F)
+            {
+                entityarrow.setIsCritical(true);
+            }
+
+            int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, stack);
+
+            if (k > 0)
+            {
+                entityarrow.setDamage(entityarrow.getDamage() + (double)k * 0.5D + 0.5D);
+            }
+
+            int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, stack);
+
+            if (l > 0)
+            {
+                entityarrow.setKnockbackStrength(l);
+            }
+
+            if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, stack) > 0)
+            {
+                entityarrow.setFire(100);
+            }
+
+            stack.damageItem(1, playerIn);
+            worldIn.playSoundAtEntity(playerIn, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+
+            if (flag)
+            {
+                entityarrow.canBePickedUp = 2;
+            }
+            else
+            {
+                playerIn.inventory.consumeInventoryItem(Items.arrow);
+            }
+
+            playerIn.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
+
+            if (!worldIn.isRemote)
+            {
+                worldIn.spawnEntityInWorld(entityarrow);
+            }
+        }
+    }
 }
--- src/main/java/com/lucidsage/morebows/MoreBows.java
+++ src/main/java/com/lucidsage/morebows/MoreBows.java
@@ -4,6 +4,7 @@ import com.lucidsage.morebows.item.IBow;
 import com.lucidsage.morebows.item.Bow;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -27,7 +28,7 @@ public class MoreBows
     @EventHandler
     public void preinit(FMLPreInitializationEvent event)
     {
-        bows.add(new Bow("stoneBow", 484, new Object[]{
+        bows.add(new Bow("stoneBow", 484, 20.0F, Item.ToolMaterial.STONE.getEnchantability(), new Object[]{
                 " $*",
                 "#(*",
                 " $*",
@@ -37,12 +38,31 @@ public class MoreBows
                 '(', Items.bow
         }));
 
-        bows.add(new Bow("ironBow", 550, new Object[]{
+        bows.add(new Bow("ironBow", 550, 16.0F, Item.ToolMaterial.IRON.getEnchantability(), new Object[]{
                 " $*",
                 "$(*",
                 " $*",
                 '*', Items.string,
                 '$', Items.iron_ingot,
+                '(', Items.bow
+        }));
+
+        bows.add(new Bow("goldBow", 68, 10.0F, Item.ToolMaterial.GOLD.getEnchantability(), new Object[]{
+                " $*",
+                "$(*",
+                " $*",
+                '*', Items.string,
+                '$', Items.gold_ingot,
+                '(', Items.bow
+        }));
+
+        bows.add(new Bow("diamondBow", 1016, 10.0F, Item.ToolMaterial.EMERALD.getEnchantability(), new Object[]{
+                " $*",
+                "#(*",
+                " $*",
+                '#', Items.iron_ingot,
+                '*', Items.string,
+                '$', Items.diamond,
                 '(', Items.bow
         }));
     }
