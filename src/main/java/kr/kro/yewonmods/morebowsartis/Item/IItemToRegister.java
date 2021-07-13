c6e9df7d8048b974113f93a4f23aac94da88e349
lmg4497 <86336662+yaeyewon112@users.noreply.github.com>
2021-07-13 12:07:04 +0900

    Primary Cleanup

--- src/main/java/kr/kro/yewonmods/morebowsartis/Item/IItemToRegister.java
+++
@@ -1,8 +0,0 @@
-package kr.kro.yewonmods.morebowsartis.Item;
-
-import net.minecraft.client.renderer.ItemModelMesher;
-
-public interface IItemToRegister {
-    void init();
-    void clientInit(ItemModelMesher itemModelMesher);
-}
